cf682db513ec0de8cfeba01468ac0c12bd6e1aad
Josh M <cyrex562@gmail.com>
2020-09-28 21:00:44 -0400

    added read_word function and mmap read

--- src/dump.rs
+++ src/dump.rs
@@ -1,7 +1,15 @@
+use mapr::Mmap;
+use mapr::MmapOptions;
 use std::{fs::{self, File}, path::Path};
 
 use crate::multi_error::MultiError;
 
+
+pub fn read_word(mmap: &Mmap, offset: usize) -> Result<[u8;2], MultiError> {
+    let read_bytes: [u8;2] = [0, 0]; 
+    read_bytes.clone_from_slice(mmap[0..2]);
+    Ok(read_bytes)
+}
 
 
 pub fn dump_file(file: &str) -> Result<(), MultiError> {
@@ -13,5 +21,7 @@ pub fn dump_file(file: &str) -> Result<(), MultiError> {
     let stats = fs::metadata(&path)?;
 
     // memory map the file
+    let mmap = unsafe { MmapOptions::new().map(&file)?};
     
+    let magic = read_word(&mmap, 0)?;
 }
